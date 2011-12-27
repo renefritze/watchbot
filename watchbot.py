e02015c29441aae9d84421eb890d014914b189fa
Rene Milk <rene.milk@uni-muenster.de>
2011-12-27 18:16:34 +0100

    fix pm watch

--- watchbot.py
+++ watchbot.py
@@ -15,6 +15,7 @@ class Main(IPlugin):
 	def __init__(self, name, tasclient):
 		IPlugin.__init__(self, name, tasclient)
 		self.admins = []
+		self.bots = []
 		self.waiting_for_reply = []
 		#this equals 130s since ping time is hardcoded to 10s
 		self.when_killed_wait = 13
@@ -23,11 +24,14 @@ class Main(IPlugin):
 		msg = 'bot %s not found ckilling pidfile %s'%(bot.name,bot.pidfile)
 		self.logger.error(msg)
 		os.system('/usr/local/bin/ckill %s'%bot.pidfile)
+		bot.when_killed = 1
 		for admin in self.admins:
 			self.tasclient.saypm(admin, msg)
 		
 	def onpong(self):
 		self.logger.debug("pong")
+		for bot in self.waiting_for_reply:
+			self._killbot(bot) 
 		for bot in self.bots:
 			if not bot.name in self.tasclient.users:
 				if bot.when_killed == 0:
@@ -39,14 +43,14 @@ class Main(IPlugin):
 					pass
 			else:
 				bot.when_killed = 0
-				self.waiting_for_reply.append( bot.name )
+				self.waiting_for_reply.append(bot)
 				self.tasclient.saypm( bot.name, bot.pm_cmd )
 		
 			
 	def onsaidprivate(self, user, message):
 		#clear user from queue
 		if message != '':
-			self.waiting_for_reply = filter (lambda a: a == user, self.waiting_for_reply)
+			self.waiting_for_reply = filter (lambda a: a.name != user, self.waiting_for_reply)
 			
 	def onload(self, tasc):
 		self.app = tasc.main
