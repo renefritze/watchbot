00fa57031dba4a06e66aa0c8de436243ebb981cb
Rene Milk <rene.milk@uni-muenster.de>
2011-12-27 12:46:34 +0100

    actually call ckill

--- watchbot.py
+++ watchbot.py
@@ -9,9 +9,15 @@ class Main(IPlugin):
 		IPlugin.__init__(self, name, tasclient)
 		self.admins = []
 
-	def oncommandfromserver(self, command, args, socket):
-		pass
+	def onpong(self):
+		self.logger.debug("pong")
+		for (user,pidfile) in self.bots:
+			if not user in self.tasclient.users:
+				self.logger.error('bot %s not found ckilling pidfile %s'%(user,pidfile))
+				os.system('/usr/local/bin/ckill %s'%pidfile)
 
 	def onload(self, tasc):
 		self.app = tasc.main
 		self.admins = self.app.config.GetOptionList('tasbot', "admins")
+		self.bots = self.app.config.items('watchbot') 
+
